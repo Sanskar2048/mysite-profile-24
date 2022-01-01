2561989a81973f4c6c6b0e549ced51ac0be803f6
sanskar2048 <ss9814@srmist.edu.in>
2022-01-01 14:30:35 +0530

    json

--- index.js
+++ index.js
@@ -17,13 +17,11 @@ app.use(expressLayouts)
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect('mongodb+srv://user:sanskar@cluster0.abi1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true , useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error' ,error => console.error(error))
 db.once('open', () =>console.log('connected to mongoose'))
-console.log('name')
-
 
 const homeRouter = require('./routers/home')
 app.use('/home',homeRouter)
