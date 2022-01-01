89189e2fa10a0ae3eaaf84671085015932143ca5
sanskar2048 <ss9814@srmist.edu.in>
2022-01-01 13:48:03 +0530

    database

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ app.use(expressLayouts)
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 
-
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true })
+    useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true})
 const db = mongoose.connection
 db.on('error' ,error => console.error(error))
 db.once('open', () =>console.log('connected to mongoose'))
